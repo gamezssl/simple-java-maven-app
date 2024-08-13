0fd198772401122045fd64c532f88328c43f5658
Sergio <sergio@git.gamezssl.com>
2024-08-13 20:58:33 +0200

    CHANGED job configuration.

--- DSL/javamavenDSL.groovy
+++ DSL/javamavenDSL.groovy
@@ -1,4 +1,4 @@
-job('/Learning-jobs/Udemy-Jenkins-Cero-Experto-2024/6-Jobs-DSL-Maven/2-DSL-Job-Maven-git') {
+job('/Learning-jobs/Udemy-Jenkins-Cero-Experto-2024/6-Jobs-DSL-Maven/1-DSL-Job-Maven') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
         git('https://github.com/gamezssl/simple-java-maven-app.git', 'master') { node ->
@@ -17,7 +17,7 @@ job('/Learning-jobs/Udemy-Jenkins-Cero-Experto-2024/6-Jobs-DSL-Maven/2-DSL-Job-M
         }
         shell('''
           echo "Entrega: Desplegando la aplicación" 
-          java -jar "/home/jenkins/agent/workspace/Learning-jobs/Udemy-Jenkins-Cero-Experto-2024/6-Jobs-DSL-Maven/2-DSL-Job-Maven-git/target/my-app-1.0-SNAPSHOT.jar"
+          java -jar "/home/jenkins/agent/workspace/Learning-jobs/Udemy-Jenkins-Cero-Experto-2024/6-Jobs-DSL-Maven/1-DSL-Job-Maven/target/my-app-1.0-SNAPSHOT.jar"
         ''')  
     }
     publishers {
